28fbfbaeca29fe199b384c3a967ab19a9a525c9c
Glenn Harris <harris.glenn.jr@gmail.com>
2014-08-11 21:32:54 -0700

    modified some views, no tests yet

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,13 +2,11 @@ class CustomersController < ApplicationController
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
 
   # GET /customers
-  # GET /customers.json
   def index
     @customers = Customer.all
   end
 
   # GET /customers/1
-  # GET /customers/1.json
   def show
   end
 
@@ -22,53 +20,37 @@ class CustomersController < ApplicationController
   end
 
   # POST /customers
-  # POST /customers.json
   def create
     @customer = Customer.new(customer_params)
 
-    respond_to do |format|
       if @customer.save
-        format.html { redirect_to @customer, notice: 'Customer was successfully created.' }
-        format.json { render :show, status: :created, location: @customer }
+        redirect_to @customer, notice: 'Customer was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @customer.errors, status: :unprocessable_entity }
+        render :new
       end
-    end
   end
 
   # PATCH/PUT /customers/1
-  # PATCH/PUT /customers/1.json
   def update
-    respond_to do |format|
-      if @customer.update(customer_params)
-        format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
-        format.json { render :show, status: :ok, location: @customer }
-      else
-        format.html { render :edit }
-        format.json { render json: @customer.errors, status: :unprocessable_entity }
-      end
+    if @customer.update(customer_params)
+      format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
+    else
+      format.html { render :edit }
     end
   end
 
   # DELETE /customers/1
-  # DELETE /customers/1.json
   def destroy
     @customer.destroy
-    respond_to do |format|
-      format.html { redirect_to customers_url, notice: 'Customer was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+      redirect_to customers_url, notice: 'Customer was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_customer
       @customer = Customer.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def customer_params
-      params[:customer]
+      params.require(:customer).permit(:name, :address1, :address2,:city,:state,:zip)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'customers#index'
   resources :widgets
 
   resources :orders
