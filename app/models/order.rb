82b619206463bc6bb1dc4f0b283c5a41a9a0b965
Glenn Harris <harris.glenn.jr@gmail.com>
2014-08-25 22:42:10 -0700

    installed aasm state machine gem. created statuses on order.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,36 @@
 class Order < ActiveRecord::Base
+  include AASM
+
   belongs_to :customer
   has_many :widgets
 
   validates :customer_id, :quantity, presence: true
+
+  scope :open_orders, -> { where(status: 'open')}
+  scope :already_assigned, -> { where(status: 'awaiting_shipment')}
+  scope :already_shipped, -> { where(status: 'shipped')}
+  scope :closed_orders, -> { where(status: 'closed')}
+
+  aasm :column => 'status' do
+    state :open, :intial => true
+    state :awaiting_shipment
+    state :shipped
+    state :closed
+
+    event :assign do
+      transitions :from => :open, :to => :awaiting_shipment
+    end
+
+    event :ship do
+      transitions :from => :awaiting_shipment, :to => :shipped
+    end
+
+    event :delivered do
+      transitions :from => :shipped, :to => :closed
+    end
+  end #end aasm
+
+  def self.statuses 
+    self.aasm.states.map(&:name)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823234614) do
+ActiveRecord::Schema.define(version: 20140826050843) do
 
   create_table "customers", force: true do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140823234614) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "quantity"
+    t.string   "status"
   end
 
   add_index "orders", ["customer_id"], name: "index_orders_on_customer_id"
