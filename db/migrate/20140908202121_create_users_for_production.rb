eae776a437bb403f001bf2a3ba13138916ffe5ab
Glenn Harris <harris.glenn.jr@gmail.com>
2014-09-08 13:27:27 -0700

    more rollout

--- db/migrate/20140908202121_create_users_for_production.rb
+++ db/migrate/20140908202121_create_users_for_production.rb
@@ -1,6 +1,6 @@
 class CreateUsersForProduction < ActiveRecord::Migration
   def change
-    create_table :users_for_productions do |t|
+    create_table :users do |t|
       t.string :first_name
       t.string :last_name
     end
